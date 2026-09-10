798179bef812bc88cfda2ea62fbf127f5b9b66c5
mariyahh326-cpu <mariyahh326@gmail.com>
2026-09-10 18:28:42 +0600

    Add files via upload

---
+++ Controls/profileControls.php
@@ -0,0 +1,32 @@
+<?php
+
+session_start();
+
+require_once "../Models/userModels.php";
+
+if($_SERVER["REQUEST_METHOD"] == "POST")
+{
+    $u_id = $_SESSION["u_id"];
+
+    $game_type = $_POST["game_type"];
+    $ranking = $_POST["ranking"];
+    $social_link = $_POST["social_link"];
+
+    $result = updateProfile(
+        $u_id,
+        $game_type,
+        $ranking,
+        $social_link
+    );
+
+    if($result)
+    {
+        echo "Profile Updated Successfully!";
+    }
+    else
+    {
+        echo "Profile Update Failed!";
+    }
+}
+
+?>
